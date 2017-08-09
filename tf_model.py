78eac7cbe4650c889328ed77bc86de56d6903ab5
jackyzha0 <jacky.zhao2019@stgeorges.bc.ca>
2017-08-09 15:36:44 -0700

    Aug 9 Commit Added proper function documentation

--- tf_model.py
+++ tf_model.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import glob
 import librosa
+import string
 import numpy as np
 import librosa.display as lib_disp
 import librosa.feature as lib_feat
@@ -12,10 +13,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 #TODO
 """
--Finish Batching
--Add Classes
--Add blank symbol
--Implement CTC
+-Add proper documentation
+-Finish Phoneme Batching
+-Fix Label Timing
+    -Add blank '__\n' for zeroes duration
 -Complete namescopes for Tensorflow
 -Implement Connectionist Temporal Classification as
 an optimization function
@@ -30,18 +31,22 @@ batchsize = 10
 preprocess = 1
 learning_rate = 0.001
 window_cutoff = 5
+sample_rate = 32000
 #0th indice +  space + blank label = 28 characters
 num_classes = ord('z') - ord('a') + 3
 
 def load_dir(fp):
     """
-    Load raw data into arrays
-    fp is a string path
-    Returns array of loaded files
-    loaded[0] = sound
-    loaded[1] = phonemes
-    loaded[2] = words
-    loaded[3] = text
+    Summary:
+        Load raw paths data into arrays
+    Parameters:
+        fp : string path to data
+    Output:
+        Returns array of loaded files
+        loaded[0] = sound
+        loaded[1] = phonemes
+        loaded[2] = words
+        loaded[3] = text
     """
     with tf.name_scope('raw_data'):
         ind = 0
@@ -80,17 +85,22 @@ def load_dir(fp):
 
 def features(rawsnd, num) :
     """
-    1 second = 32 units
-    Compute and return a (num+28,(window_cutoff*32))-dimensional Tensorflow feature vector, and length in
-        num Amount of Mel Frequency Ceptral Coefficients
-        12 Chromagrams
-        7 bands of Spectral Contrast
-        6 bands of Tonal Centroid Features (Tonnetz)
-        Zero Crossing Rate
-        Spectral Rolloff
-        Spectral Centroid
+    Summary:
+        Compute audio features
+    Parameters:
+        rawsnd : array with string paths to .wav files
+        num : numbers of mfccs to compute
+    Output:
+        Return a (num+28,window_cutoff*32)-dimensional Tensorflow feature vector, and length in
+        *num Amount of Mel Frequency Ceptral Coefficients
+        *12 Chromagrams
+        *7 bands of Spectral Contrast
+        *6 bands of Tonal Centroid Features (Tonnetz)
+        *Zero Crossing Rate
+        *Spectral Rolloff
+        *Spectral Centroid
     """
-    x, sample_rate = librosa.load(rawsnd,sr=16000, duration=window_cutoff)
+    x, _ = librosa.load(rawsnd,sr=sample_rate, duration=window_cutoff)
     s_tft = np.abs(librosa.stft(x))
     ft = lib_feat.mfcc(y=x, sr=sample_rate, n_mfcc=num)
     ft = np.append(ft,lib_feat.chroma_stft(S=s_tft, sr=sample_rate),axis=0)
@@ -99,43 +109,57 @@ def features(rawsnd, num) :
     ft = np.append(ft,lib_feat.zero_crossing_rate(y=x),axis=0)
     ft = np.append(ft,lib_feat.spectral_rolloff(y=x,sr=sample_rate),axis=0)
     ft = np.append(ft,lib_feat.spectral_centroid(y=x,sr=sample_rate),axis=0)
-    z = np.zeros((num+12+7+6+3,(window_cutoff*32)-ft.shape[1]))
+    z = np.zeros((num+12+7+6+3,(window_cutoff*((sample_rate/1000)*2))-ft.shape[1]))
     ft = np.concatenate((ft,z),axis=1)
+    #print(ft[13].astype(np.float16))
     return (ft)
 
 def preprocess(rawsnd) :
     """
+    #INCOMPLETE
     If preprocess == 1, add additional white noise and
     random sample from Urban8K
     """
 
 def BLSTM():
     """
+    #INCOMPLETE
     """
 
 def batch(data,truth_label,index):
     """
-    Outputs feed dictionary for training
-    Form of:
-    feed_dict = {<placeholder>: <tensor values>}
+    Summary:
+        Creates a tuple of audio features and truth labels
+    Parameters:
+        data : array of string paths to .wav files
+        truth_label : array of strings containing phonemes, start times, and end times
+        index : integer to start batching from
+    Output:
+        2 numpy arrays
+        *Feature vector
+            Dimensions(num_mfccs+28,320)
+        *Label vector
     """
     print('Constructing input dictionary of size %d' % len(data))
-    feature_vec = []
+    feature_vec = np.empty((num_mfccs+28,window_cutoff*((sample_rate/1000)*2)))
     label_vec = []
-    largest = 0;
     if index > (len(data)-batchsize)-(len(data)%batchsize):
         raise ValueError('Out of Bounds')
     else:
         for i in range(index,index+batchsize):
-            feature_vec.append(features(data[i],num_mfccs))
+            np.append(feature_vec,features(data[i],num_mfccs),axis=0)
+            #print(feature_vec)
             for f in truth_label[i]:
-                label_vec.append(f[1])
-    #return dict(zip(feature_vec, label_vec))
-    return 0
+                label_vec.append(f[2])
+                print(f[2])
+    print(len(label_vec))
+    return feature_vec, lv
 
 def plot(spec):
     """
-    Creates librosa plot given output of features(_,_)
+    Summary:
+        Creates and shows librosa plot given output of features
+
     """
     plt.subplot(5,1,1)
     librosa.display.specshow(spec[0], x_axis='time')
@@ -148,6 +172,7 @@ def plot(spec):
 
 def train():
     """
+    #INCOMPLETE
     Trains the model
     """
 
