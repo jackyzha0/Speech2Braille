8aff657d110d53d4ab886a200e98a3738cb4fc2f
jackyzha0 <jacky.zhao2019@stgeorges.bc.ca>
2017-12-03 18:57:49 -0800

    Dec. 3 Commit 3 Bug persists

--- tf_model.py
+++ tf_model.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import os
 import sys
 import data_util
+import numpy as np
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 #PARAMS
 FLAGS = None
@@ -95,8 +96,67 @@ with graph.as_default():
 
     ler = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32),targets))
 
+def pad_sequences(sequences, maxlen=None, dtype=np.float32,
+                  padding='post', truncating='post', value=0.):
+    '''Pads each sequence to the same length: the length of the longest
+    sequence.
+        If maxlen is provided, any sequence longer than maxlen is truncated to
+        maxlen. Truncation happens off either the beginning or the end
+        (default) of the sequence. Supports post-padding (default) and
+        pre-padding.
+        Args:
+            sequences: list of lists where each element is a sequence
+            maxlen: int, maximum length
+            dtype: type to cast the resulting sequence.
+            padding: 'pre' or 'post', pad either before or after each sequence.
+            truncating: 'pre' or 'post', remove values from sequences larger
+            than maxlen either in the beginning or in the end of the sequence
+            value: float, value to pad the sequences to the desired value.
+        Returns
+            x: numpy array with dimensions (number_of_sequences, maxlen)
+            lengths: numpy array with the original sequence lengths
+    '''
+    lengths = np.asarray([len(s) for s in sequences], dtype=np.int64)
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    # take the sample shape from the first non empty sequence
+    # checking for consistency in the main loop below.
+    sample_shape = tuple()
+    for s in sequences:
+        if len(s) > 0:
+            sample_shape = np.asarray(s).shape[1:]
+            break
+
+    x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError('Truncating type "%s" not understood' % truncating)
+
+        # check `trunc` has expected shape
+        trunc = np.asarray(trunc, dtype=dtype)
+        if trunc.shape[1:] != sample_shape:
+            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+                             (trunc.shape[1:], idx, sample_shape))
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError('Padding type "%s" not understood' % padding)
+    return x, lengths
+
 # Launch the graph
-with tf.Session() as sess:
+with tf.Session(graph=graph) as sess:
     tf.global_variables_initializer().run()
 
     #Load paths
@@ -115,7 +175,9 @@ with tf.Session() as sess:
             indexes = [i % batchsize for i in range(j * batchsize, (j + 1) * batchsize)]
             batch_train_targets = data_util.sparse_tuple_from(minibatch_targets[j])
             batch_train_inputs = minibatch[indexes]
-            feed = {inputs: batch_train_inputs, targets: batch_train_targets}
+            print(batch_train_inputs.shape)
+            batch_train_inputs, batch_train_seq_len = pad_sequences(batch_train_inputs)
+            feed = {inputs: batch_train_inputs, targets: batch_train_targets, seq_len: batch_train_seq_len}
 
             #Batch
             batch_cost, _ = sess.run([cost, optimizer], feed)
