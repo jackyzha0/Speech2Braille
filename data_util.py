c927b7a3bbece792c43855e46d834fe6f72aacc4
jackyzha0 <jacky.zhao2019@stgeorges.bc.ca>
2017-12-25 15:39:26 -0500

    Dec. 25 Commit 3

--- data_util.py
+++ data_util.py
@@ -5,6 +5,7 @@ import time
 print(time.strftime('[%H:%M:%S]'), 'Starting data loader... ')
 print(time.strftime('[%H:%M:%S]'), 'Importing libraries... ')
 import numpy as np
+import string
 print('[OK] numpy ')
 import librosa.display as lib_disp
 import librosa.feature as lib_feat
@@ -14,37 +15,14 @@ import tensorflow as tf
 print('[OK] tensorflow ')
 import glob
 print('[OK] glob ')
+from PIL import Image
+import scipy
 
 batchsize = -1
 num_mfccs = -1
 num_classes = -1
-path_phonetable = '/home/jacky/2kx/Spyre/git/phon_table.txt'
-path_replacekey = '/home/jacky/2kx/Spyre/git/replace_key.txt'
-phn_lookup = []
-replacekey = {}
-
-print(time.strftime('[%H:%M:%S]'), 'Constructing Phone Conversion Table...')
-with open(path_phonetable) as f:
-    for line in f:
-        tmp_phn = line.split("\n")[0]
-        phn_lookup.append(tmp_phn)
-    print(phn_lookup)
-print(time.strftime('[%H:%M:%S]'),'Loaded phone table')
-with open(path_replacekey) as f:
-    key = []
-    element = []
-    for line in f:
-        key.append(line.split(" ")[0])
-        element.append((line.split(" ")[1:][0]).strip())
-    print(key,element)
-    replacekey = dict(zip(key,element))
-    print(replacekey)
-print(time.strftime('[%H:%M:%S]'),'Loaded phomeme replacement key')
 
 print(time.strftime('[%H:%M:%S]'), 'Loading helper functions...')
-
-def getClass():
-    return len(phn_lookup)-len(replacekey.keys())
 
 def setParams(_batchsize, _num_mfccs, _num_classes):
     """Set Training Parameters
@@ -80,6 +58,27 @@ def sparse_tuple_from(sequences, dtype=np.int32):
 
     return indices, values, shape
 
+def jpg_image_to_array(image_path):
+    img = Image.open(image_path).convert('L')  # convert image to 8-bit grayscale
+    WIDTH, HEIGHT = img.size
+    data = list(img.getdata()) # convert image data to a list of integers
+    # convert that to 2D list (list of lists of integers)
+    data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
+    return np.expand_dims(data, axis=4)
+
+def r_saveImg(arr):
+    n=0
+    for i in arr:
+        im = scipy.misc.toimage(i)
+        im.save('/home/jacky/2kx/Spyre/git/r_img/'+str(n)+'.jpg')
+        n+=1
+def saveImg(arr):
+    n=0
+    for i in arr:
+        im = scipy.misc.toimage(i)
+        im.save('/home/jacky/2kx/Spyre/git/img/'+str(n)+'.jpg')
+        n+=1
+
 def features(rawsnd, num) :
     """Compute num amount of audio features of a sound
     Args:
@@ -96,27 +95,8 @@ def features(rawsnd, num) :
         *Spectral Centroid"""
     x, sample_rate = librosa.load(rawsnd)
     s_tft = np.abs(librosa.stft(x))
-    ft = lib_feat.mfcc(y=x, sr=sample_rate, n_mfcc=num)
-    ft = np.append(ft,lib_feat.chroma_stft(S=s_tft, sr=sample_rate),axis=0)
-    ft = np.append(ft,lib_feat.spectral_contrast(S=s_tft,sr=sample_rate),axis=0)
-    ft = np.append(ft,lib_feat.tonnetz(y=librosa.effects.harmonic(x), sr=sample_rate),axis=0)
-    ft = np.append(ft,lib_feat.zero_crossing_rate(y=x),axis=0)
-    ft = np.append(ft,lib_feat.spectral_rolloff(y=x,sr=sample_rate),axis=0)
-    ft = np.append(ft,lib_feat.spectral_centroid(y=x,sr=sample_rate),axis=0)
-    ft = np.swapaxes(ft,0,1)
+    ft = lib_feat.mfcc(y=x, sr=sample_rate, n_mfcc=num).T
     return (ft)
-
-def phn_to_int(inp):
-    if inp=='-':
-        return 0
-    else:
-        return phn_lookup.index(inp)+1
-
-def check_phone_conversion(inp):
-    if inp in replacekey.keys():
-        return replacekey[inp]
-    else:
-        return inp
 
 def load_dir(fp):
     """Load raw paths data into arrays
@@ -139,30 +119,20 @@ def load_dir(fp):
                     ind+=1
                     if (".wav" in __file):
                         raw_audio.append(__file)
-                    if (".PHN" in __file):
-                        with open(__file) as f:
-                            tmp_phn_file = []
-                            for line in f:
-                                tmp_phn = line.split(" ")
-                                tmp_phn = phn_to_int(check_phone_conversion(tmp_phn[2][:-1]))
-                                if tmp_phn != 0:
-                                    tmp_phn_file.append(tmp_phn)
-                        print(time.strftime('[%H:%M:%S]'),'Phone file',__file[35:],'loaded, size',len(tmp_phn_file))
-                        phonemes.append(tmp_phn_file)
-                    if (".WRD" in __file):
-                        with open(__file) as f:
-                            tmp_wrd_file = []
-                            for line in f:
-                                tmp_wrd = line.split(" ")
-                                tmp_wrd_file.append(tmp_wrd)
-                        words.append(tmp_wrd_file)
                     if (".TXT" in __file):
                         with open(__file) as f:
                             for line in f:
                                 res = ''.join([i for i in line if not i.isdigit()])
-                        text.append(res)
+                        res = (list(res[2:-1].lower().translate(None, string.punctuation)))
+                        tmp_res = []
+                        for r in res:
+                            if r==' ':
+                                tmp_res.append(0)
+                            else:
+                                tmp_res.append(ord(r)-96)
+                        text.append(tmp_res)
         print(time.strftime('[%H:%M:%S]'), 'Succesfully loaded data set of size',len(raw_audio))
-        return raw_audio,phonemes,words,text,ind
+        return raw_audio,text,len(raw_audio)
 
 def next_Data(path):
     """Returns array of features for training
@@ -184,21 +154,92 @@ def next_miniBatch(index,patharr):
         features = rank 3 tensor of batchsize * maxsize * num_features+28
     """
     minibatch = []
-    for j in range(0,batchsize):
-        tmp = next_Data(patharr[index+j])
+    for j in index:
+        tmp = next_Data(patharr[j])
         minibatch.append(np.array(tmp[0]))
-        print(time.strftime('[%H:%M:%S]'), 'Passed input tensor with rank...',np.array(tmp[0]).shape,j+1,'/',batchsize)
+        #print(time.strftime('[%H:%M:%S]'), 'Passed input tensor',batchsize)
     minibatch = np.array(minibatch)
-    #minibatch = np.swapaxes(minibatch,2,1)
-    print(time.strftime('[%H:%M:%S]'), 'Succesfully loaded minibatch of rank',minibatch.shape)
-    return minibatch
+    saveImg(minibatch)
+    return np.asarray(minibatch)
 def next_target_miniBatch(index,patharr):
     minibatch = []
-    for j in range(0,batchsize):
-        tmp = patharr[index+j]
+    for j in index:
+        tmp = patharr[j]
         tmp_k = []
         for k in range(0,len(tmp)):
             tmp_k.append(int(tmp[k]))
         minibatch.append(np.array(tmp_k))
-        print(time.strftime('[%H:%M:%S]'), 'Passed target tensor of rank',np.array(tmp_k).ndim,j+1,'/',batchsize)
+        #print(time.strftime('[%H:%M:%S]'), 'Passed target tensor',batchsize)
     return np.asarray(minibatch)
+
+def pad_sequences(sequences, maxlen=None, dtype=np.float32,
+                  padding='post', truncating='post', value=0.):
+    '''Pads each sequence to the same length: the length of the longest
+    sequence.
+        If maxlen is provided, any sequence longer than maxlen is truncated to
+        maxlen. Truncation happens off either the beginning or the end
+        (default) of the sequence. Supports post-padding (default) and
+        pre-padding.
+        Args:
+            sequences: list of lists where each element is a sequence
+            maxlen: int, maximum length
+            dtype: type to cast the resulting sequence.
+            padding: 'pre' or 'post', pad either before or after each sequence.
+            truncating: 'pre' or 'post', remove values from sequences larger
+            than maxlen either in the beginning or in the end of the sequence
+            value: float, value to pad the sequences to the desired value.
+        Returns
+            x: numpy array with dimensions (number_of_sequences, maxlen)
+            lengths: numpy array with the original sequence lengths
+    '''
+    lengths = np.asarray([len(s) for s in sequences], dtype=np.int64)
+
+    nb_samples = len(sequences)
+    if maxlen is None:
+        maxlen = np.max(lengths)
+
+    # take the sample shape from the first non empty sequence
+    # checking for consistency in the main loop below.
+    sample_shape = tuple()
+    for s in sequences:
+        if len(s) > 0:
+            sample_shape = np.asarray(s).shape[1:]
+            break
+
+    x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
+    for idx, s in enumerate(sequences):
+        if len(s) == 0:
+            continue  # empty list was found
+        if truncating == 'pre':
+            trunc = s[-maxlen:]
+        elif truncating == 'post':
+            trunc = s[:maxlen]
+        else:
+            raise ValueError('Truncating type "%s" not understood' % truncating)
+
+        # check `trunc` has expected shape
+        trunc = np.asarray(trunc, dtype=dtype)
+        if trunc.shape[1:] != sample_shape:
+            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+                             (trunc.shape[1:], idx, sample_shape))
+
+        if padding == 'post':
+            x[idx, :len(trunc)] = trunc
+        elif padding == 'pre':
+            x[idx, -len(trunc):] = trunc
+        else:
+            raise ValueError('Padding type "%s" not understood' % padding)
+    return x, lengths
+
+def fake_data(num_examples, num_features, num_labels, min_size = 10, max_size=100):
+    np.random.seed(0)
+    # Generating different timesteps for each fake data
+    timesteps = np.random.randint(min_size, max_size, (num_examples,))
+    # Generating random input
+    inputs = np.asarray([np.random.randn(t, num_features).astype(np.float32) for t in timesteps])
+    #inputs = np.asarray([np.ones((t, num_features))*255 for t in timesteps])
+
+    # Generating random label, the size must be less or equal than timestep in order to achieve the end of the lattice in max timestep
+    labels = np.asarray([np.random.randint(0, num_labels, np.random.randint(1, inputs[i].shape[0], (1,))).astype(np.int64) for i, _ in enumerate(timesteps)])
+    r_saveImg(inputs)
+    return inputs,labels
--- tf_model.py
+++ tf_model.py
@@ -1,29 +1,45 @@
 # !/usr/local/bin/python
 from __future__ import print_function
 import time
-import datetime
 print(time.strftime('[%H:%M:%S]'), 'Starting network... ')
 import tensorflow as tf
+import datetime
 import os
 import sys
 import data_util
 import numpy as np
+from PIL import Image
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 #PARAMS
 FLAGS = None
-path = '/home/jacky/2kx/Spyre/__data/TIMIT/*/*/*'
+#path = '/home/jacky/2kx/Spyre/__data/TIMIT/*/*/*'
 #path = '/home/jacky/2kx/Spyre/pract_data/*'
-#path = '/home/jacky/2kx/Spyre/larger_pract/*/*'
+path = '/home/jacky/2kx/Spyre/larger_pract/*/*'
 logs_path = '/home/jacky/2kx/Spyre/git/totalsummary/logs/'+datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
 num_mfccs = 13
-batchsize = 1
-num_hidden = 100
-learning_rate = 5e-3
-momentum = 0.9
-num_layers = 2
 prevcost = 0
-#0th indice + End indice + space + blank label = 27 characters
-num_classes = 63
+
+num_classes = 28
+
+num_hidden = 50
+learning_rate = 1e-3
+momentum = 0.9
+num_layers = 1
+
+# Constants
+SPACE_TOKEN = '<space>'
+SPACE_INDEX = 0
+FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
+
+print(time.strftime('[%H:%M:%S]'), 'Parsing directory... ')
+dr = data_util.load_dir(path)
+datasetsize = len(dr[0])
+lr = dr[1]
+
+num_examples = 64#dr[2]
+num_epochs = 40
+batchsize = 2
+num_batches_per_epoch = int(num_examples/batchsize)
 
 print(time.strftime('[%H:%M:%S]'), 'Loading network functions... ')
 graph = tf.Graph()
@@ -32,18 +48,25 @@ with graph.as_default():
       return tf.contrib.rnn.BasicLSTMCell(num_hidden)
 
     #Network Code is heavily influenced by igormq's ctc_tensorflow example
+    with tf.name_scope('inputLength'):
+        seq_len = tf.placeholder(tf.int32, [None])
+
     with tf.name_scope('input'):
-        inputs = tf.placeholder(tf.float32, [batchsize, None, num_mfccs+28])
+        inputs = tf.placeholder(tf.float32, [None, None, num_mfccs])
         targets = tf.sparse_placeholder(tf.int32)
         tf.summary.histogram("input",inputs)
         tf.summary.histogram("targets",tf.sparse_to_dense(targets.indices,targets.dense_shape,targets.values))
 
-    with tf.name_scope('inputLength'):
-        seq_len = tf.placeholder(tf.int32, [None])
+    with tf.name_scope('mfccs'):
+        t = tf.placeholder(tf.float32, [None, None, None,None])
+        tf.summary.image("mfccs",t,batchsize)
 
     # Stacking rnn cells
     with tf.name_scope('cellStack'):
-        stack = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(num_layers)])
+        #stack = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(num_layers)])
+        cell = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True)
+        stack = tf.contrib.rnn.MultiRNNCell([cell] * num_layers,
+                                            state_is_tuple=True)
 
     # The second output is the last state and we will not use that
     outputs, _ = tf.nn.dynamic_rnn(stack, inputs, seq_len, dtype=tf.float32)
@@ -55,16 +78,17 @@ with graph.as_default():
         outputs = tf.reshape(outputs, [-1, num_hidden])
 
     with tf.name_scope('weights'):
-        #initializer = tf.contrib.layers.xavier_initializer()
-        W = tf.Variable(tf.truncated_normal([num_hidden,num_classes],stddev=0.01))
+        initializer = tf.contrib.layers.xavier_initializer()
+        W = tf.Variable(initializer([num_hidden,num_classes]))
+        #W = tf.Variable(tf.truncated_normal([num_hidden,num_classes],stddev=1))
+        #W = tf.Variable(tf.truncated_normal([num_hidden,num_classes],stddev=0.1))
         #W = tf.Variable(tf.random_normal([num_hidden, num_classes]))
         tf.summary.histogram('weightsHistogram', W)
-    # Zero initialization
     with tf.name_scope('biases'):
         b = tf.Variable(tf.constant(0., shape=[num_classes]))
         tf.summary.histogram('biasesHistogram', b)
 
-    with tf.name_scope('outputTransform'):
+    with tf.name_scope('logits'):
         # Doing the affine projection
         logits = tf.matmul(outputs, W) + b
         # Reshaping back to the original shape
@@ -72,88 +96,22 @@ with graph.as_default():
         # Time major
         logits = tf.transpose(logits, (1, 0, 2))
     with tf.name_scope('loss'):
-        loss = tf.reduce_sum(tf.nn.ctc_loss(targets, logits, seq_len, ignore_longer_outputs_than_inputs=True))
+        loss = tf.reduce_mean(tf.nn.ctc_loss(targets, logits, seq_len, ignore_longer_outputs_than_inputs=True))
     with tf.name_scope('cost'):
         cost = tf.reduce_mean(loss)
     tf.summary.scalar("cost", cost)
-    #var_summaries(loss)
     with tf.name_scope('optimizer'):
-        #optimizer = tf.train.MomentumOptimizer(learning_rate,momentum).minimize(cost)
-        optimizer = tf.train.AdamOptimizer().minimize(cost)
-
+        optimizer = tf.train.MomentumOptimizer(learning_rate,momentum).minimize(cost)
+        #optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
     with tf.name_scope('decoder'):
         decoded, log_prob = tf.nn.ctc_greedy_decoder(logits, seq_len)
+        #decoded = tf.to_int32(ctc.ctc_beam_search_decoder(logits, seq_len)[0][0])
         tf.summary.histogram("outputs",tf.sparse_to_dense(decoded[0].indices,decoded[0].dense_shape,decoded[0].values))
-        tf.summary.histogram("log_prob",log_prob)
     with tf.name_scope('LER'):
         ler = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32),targets))
-    tf.summary.scalar("LER", cost)
+    tf.summary.scalar("LER", ler)
 
     merged = tf.summary.merge_all()
-
-def pad_sequences(sequences, maxlen=None, dtype=np.float32,
-                  padding='post', truncating='post', value=0.):
-    '''Pads each sequence to the same length: the length of the longest
-    sequence.
-        If maxlen is provided, any sequence longer than maxlen is truncated to
-        maxlen. Truncation happens off either the beginning or the end
-        (default) of the sequence. Supports post-padding (default) and
-        pre-padding.
-        Args:
-            sequences: list of lists where each element is a sequence
-            maxlen: int, maximum length
-            dtype: type to cast the resulting sequence.
-            padding: 'pre' or 'post', pad either before or after each sequence.
-            truncating: 'pre' or 'post', remove values from sequences larger
-            than maxlen either in the beginning or in the end of the sequence
-            value: float, value to pad the sequences to the desired value.
-        Returns
-            x: numpy array with dimensions (number_of_sequences, maxlen)
-            lengths: numpy array with the original sequence lengths
-    '''
-    lengths = np.asarray([len(s) for s in sequences], dtype=np.int64)
-
-    nb_samples = len(sequences)
-    if maxlen is None:
-        maxlen = np.max(lengths)
-
-    # take the sample shape from the first non empty sequence
-    # checking for consistency in the main loop below.
-    sample_shape = tuple()
-    for s in sequences:
-        if len(s) > 0:
-            sample_shape = np.asarray(s).shape[1:]
-            break
-
-    x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
-    for idx, s in enumerate(sequences):
-        if len(s) == 0:
-            continue  # empty list was found
-        if truncating == 'pre':
-            trunc = s[-maxlen:]
-        elif truncating == 'post':
-            trunc = s[:maxlen]
-        else:
-            raise ValueError('Truncating type "%s" not understood' % truncating)
-
-        # check `trunc` has expected shape
-        trunc = np.asarray(trunc, dtype=dtype)
-        if trunc.shape[1:] != sample_shape:
-            raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
-                             (trunc.shape[1:], idx, sample_shape))
-
-        if padding == 'post':
-            x[idx, :len(trunc)] = trunc
-        elif padding == 'pre':
-            x[idx, -len(trunc):] = trunc
-        else:
-            raise ValueError('Padding type "%s" not understood' % padding)
-    return x, lengths
-
-print(time.strftime('[%H:%M:%S]'), 'Parsing directory... ')
-dr = data_util.load_dir(path)
-datasetsize = len(dr[0])
-lr = dr[1]
 
 # Launch the graph
 with tf.Session(graph=graph) as sess:
@@ -162,52 +120,56 @@ with tf.Session(graph=graph) as sess:
     tf.global_variables_initializer().run()
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata = tf.RunMetadata()
-
     #Load paths
     print(time.strftime('[%H:%M:%S]'), 'Passing params... ')
     data_util.setParams(batchsize, num_mfccs, num_classes)
-    #Training Loop
-    train_cost = train_ler = 0
-    start = prev = time.time()
+    for curr_epoch in range(num_epochs):
+        print('>>>',time.strftime('[%H:%M:%S]'), 'Epoch',curr_epoch,'/',num_epochs)
+        train_cost = train_ler = 0
+        start = time.time()
+        for batch in range(num_batches_per_epoch):
+            # Getting the index
+            indexes = [i % num_examples for i in range(batch * batchsize, (batch + 1) * batchsize)]
+            train_targets = data_util.next_target_miniBatch(indexes,dr[1])
+            train_inputs = data_util.next_miniBatch(indexes,dr[0])
+            #train_inputs,train_targets = data_util.fake_data(num_examples,num_mfccs,num_classes-1)
+            newindex = [i % num_examples for i in range(batchsize)]
+            batch_train_inputs = train_inputs[newindex]
+            # Padding input to max_time_step of this batch
+            batch_train_inputs, batch_train_seq_len = data_util.pad_sequences(batch_train_inputs)
+            # Converting to sparse representation so as to to feed SparseTensor input
+            batch_train_targets = data_util.sparse_tuple_from(train_targets[newindex])
+            batch_train_mfccs_img = []
+            data_util.saveImg(batch_train_inputs)
+            for n in range(0,batchsize):
+                batch_train_mfccs_img.append(data_util.jpg_image_to_array('/home/jacky/2kx/Spyre/git/img/'+str(n)+'.jpg'))
+            feed = {inputs: batch_train_inputs,
+                    targets: batch_train_targets,
+                    seq_len: batch_train_seq_len,
+                    t: batch_train_mfccs_img
+                    }
+            batch_cost, _ = sess.run([cost, optimizer], feed)
+            train_cost += batch_cost*batchsize
+            train_ler += sess.run(ler, feed_dict=feed)*batchsize
+            print('      >>>',time.strftime('[%H:%M:%S]'), 'Batch',batch,'/',num_batches_per_epoch,'@Cost',batch_cost/batchsize)
+            summary = sess.run(merged, feed_dict=feed, options=run_options, run_metadata=run_metadata)
+            writer.add_summary(summary, batch+(curr_epoch*num_batches_per_epoch))
+            writer.add_run_metadata(run_metadata, 'step%03d' % int(batch+(curr_epoch*num_batches_per_epoch)))
+            writer.flush()
 
-    for i in range(0,datasetsize/batchsize):
-        #print(datasetsize,batchsize)
-        minibatch = data_util.next_miniBatch(i*batchsize,dr[0])
-        minibatch_targets = data_util.next_target_miniBatch(i*batchsize,dr[1])
-        indexes = [j % batchsize for j in range(i * batchsize, (i + 1) * batchsize)]
-        batch_train_targets = data_util.sparse_tuple_from(minibatch_targets)
-        batch_train_inputs = minibatch
-        batch_train_inputs, batch_train_seq_len = pad_sequences(batch_train_inputs)
-        print(time.strftime('[%H:%M:%S]'),'Updating Weights of batch',i)
-        feed = {inputs: batch_train_inputs, targets: batch_train_targets, seq_len: batch_train_seq_len}
-        #Batch
-        batch_cost, _ = sess.run([cost, optimizer], feed)
-        train_cost += batch_cost*batchsize
-        train_ler += sess.run(ler, feed_dict=feed)*batchsize
-        time_fm = "{:.4f} seconds"
+        # Metrics mean
+        train_cost /= num_examples
+        train_ler /= num_examples
 
+        log = "Epoch {}/{}, train_cost = {:.3f}, train_ler = {:.3f}, time = {:.3f}"
+        print(log.format(curr_epoch+1, num_epochs, train_cost, train_ler, time.time() - start))
         d = sess.run(decoded[0], feed_dict=feed)
         dense_decoded = tf.sparse_tensor_to_dense(d, default_value=-1).eval(session=sess)
-        for k, seq in enumerate(dense_decoded):
+        #print(dense_decoded)
+        for i, seq in enumerate(dense_decoded):
 
             seq = [s for s in seq if s != -1]
-            print('Sequence %d' % k)
-            print('\t Original:\n%s' % minibatch_targets[k])
-            print('\t Decoded:\n%s' % seq)
-        print(dense_decoded)
-        print(time.strftime('[%H:%M:%S]'),'Batch',i,'trained in',time_fm.format(time.time()-prev))
-        print('>>> Cost:', batch_cost)
-        print('>>> Cost Diff:', batch_cost-prevcost)
-        print('>>>',(i+1)*batchsize,'/',datasetsize,'  Estimated Training Time:',time_fm.format((datasetsize-((i+1)*batchsize))*((time.time()-prev)/4)))
-        prevcost = batch_cost
-        prev = time.time()
-        # write log
-        summary = sess.run(merged, feed_dict=feed, options=run_options, run_metadata=run_metadata)
-        writer.add_summary(summary, i)
-        writer.add_run_metadata(run_metadata, 'step%03d' % i)
-        writer.flush()
 
-    train_cost /= datasetsize
-    train_ler /= datasetsize
-    log = "Cost: {:.3f}, Label Error Rate: {:.3f}, Time taken: {:.3f}"
-    print(time.strftime('[%H:%M:%S]'),log.format(train_cost, train_ler, time.time() - start))
+            print('Sequence %d' % i)
+            print('\t Original:\n%s' % train_targets[i])
+            print('\t Decoded:\n%s' % seq)
