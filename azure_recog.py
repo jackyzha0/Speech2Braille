a2be589a5a9c20d52ba3b840e54a8202c09f6b85
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-18 14:54:08 -0700

    Fixed Azure

--- azure_recog.py
+++ azure_recog.py
@@ -1,7 +1,21 @@
 import requests
-import azure.cognitiveservices.speech as speechsdk
+import wave
+import json
 
 subscription_key = '3c86100c6a4b482b9a41552d5f05859b'
+ENDPOINT = 'https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1'
+
+def transcribe(path):
+    with open(path, 'rb') as fobj:
+        res = requests.post(url=ENDPOINT,
+                            data=fobj,
+                            params= {'language': 'en-US',
+                                'profanity': 'raw'},
+                            headers={'Ocp-Apim-Subscription-Key': subscription_key,
+                                'Content-Type': 'audio/wav; codecs=audio/pcm; samplerate=16000',
+                                'Accept': 'application/json;text/xml'}
+                            )
+    return res.json()['DisplayText']
 
 def transcribe(path):
     speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region='westus')
