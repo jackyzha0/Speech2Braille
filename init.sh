6453a24e521751ec86c2d3c5de1f5b3b5eb43bef
Jacky <jacky.zhao2019@stgeorges.bc.ca>
2019-03-18 09:48:05 -0700

    Duped echo statements for both displays

--- init.sh
+++ init.sh
@@ -19,21 +19,25 @@ fi
 
 echo -e ${clear} > /dev/tty1
 echo -e "\n${yellow}**Cleaning Up directory...${end}" > /dev/tty1
+echo -e "\n${yellow}**Cleaning Up directory...${end}"
 rm _dir/*
 python aws.py &
 BGPID2=$!
 python gpiostates.py 2>&1 > /dev/tty1&
 BGPID=$!
 echo -e "\nChecking for Button & Audio ..." > /dev/tty1
+echo -e "\nChecking for Button & Audio ..."
 while true
 do
     if [ ! -e _dir/check ] && [ -e _dir/gpio_on ]; then
         rm _dir/tmp.wav
+        echo "***** [RECORDING] *****" > /dev/tty1
         echo "***** [RECORDING] *****"
         eval $"rec -c 1 -r 48000 '_dir/tmp.wav' silence 1 0.1 5% 1 0:00:01 1% 2>&1 | cat - > /dev/tty1"
         touch _dir/check
         cp _dir/tmp.wav /media/usb1
+        echo "***** [RECORDING ENDED] *****" > /dev/tty1
         echo "***** [RECORDING ENDED] *****"
     fi
-    # sleep 1
+    sleep 0.1
 done
